7ab1863adec0becd9c3053dc7841379c72c6216e
JFKISNOTDEAD <69213932+JFKISNOTDEAD@users.noreply.github.com>
2021-10-06 10:12:28 -0300

    Add files via upload

---
+++ v2/index.js
@@ -0,0 +1,17 @@
+
+let passangers = 0
+
+function incrementPassanger(){
+    passangers = passangers + 1
+    document.getElementById("count-el").innerText = passangers
+    console.log(passangers)
+}
+
+
+function reset(){
+passangers = passangers - passangers
+    document.getElementById("count-el").innerText = passangers
+    console.log(passangers)
+}
+
+
